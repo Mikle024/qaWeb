a558bba73dcff3fd0d2f20c1442dbf351396a533
Mikhail <mikle0240@gmail.com>
2023-11-10 16:19:15 +0300

    Modified build.gradle.kts, add WebDriverManager

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,7 @@ repositories {
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:5.6.1")
     testImplementation("com.codeborne:selenide:6.17.1")
+    testImplementation("io.github.bonigarcia:webdrivermanager:5.6.2")
 }
 
 tasks.test {
